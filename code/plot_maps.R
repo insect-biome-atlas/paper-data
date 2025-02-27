ca7375c83dfb95b7dcf6a45344ddbc594f571b14
Rgoodsell <r.goodsell@sheffield.ac.uk>
2025-02-27 14:06:13 +0000

    updating maps

--- code/plot_maps.R
+++ code/plot_maps.R
@@ -13,8 +13,8 @@ library(scales)
 
 # Sweden samples ----------------------------------------------------------
 
-meta_se <- read_tsv("data/sites_metadata_se.tsv")
-meta_mg <- read_tsv("data/sites_metadata_mg.tsv")
+meta_se <- read_tsv("data/Sweden/sites_metadata_SE.tsv") 
+meta_mg <- read_tsv("data/Madagascar/sites_metadata_MG.tsv")
 
 # water bodies --------------------------------------------------------------------------------
 # map of Sweden
@@ -92,74 +92,81 @@ elev_limits <- pmax(elev_limits, 0) # Set min to 0
 
 
 # plots ---------------------------------------------------------------------------------------
+
+# Format meta-data
 meta_mg <- meta_mg  |>
             mutate(trap_habitat=recode(trap_habitat , 
-                                       "Dry_Forest" = "Dry Forest",
+                                       "Dry_Forest"          = "Dry Forest",
                                         "Montane_Rainforest" = "Wet Forest",
-                                        "Rainforest"= "Wet Forest"),
+                                        "Rainforest"         = "Wet Forest"),
                               malaise_trap_type = recode(malaise_trap_type , "Single_trap" = "Single trap"))
 
-
+# Format meta-data
 meta_se <- meta_se  |>
             mutate(malaise_trap_type = recode(malaise_trap_type , "Single_trap" = "Single trap"),
-                   NILS_mhabitat = str_to_title(NILS_mhabitat)) |> 
+                   trap_habitat = str_to_title(trap_habitat)) |> 
             drop_na(NILS_mhabitat)
 
-
-tcols <- met.brewer("Redon" , n = length(unique(meta_se$NILS_mhabitat)))
+tcols <- met.brewer("Veronese" , n = length(unique(meta_se$NILS_mhabitat)))
 
 p1 <- swe_hshade_plot + # PLot hillshaded map
-  geom_spatraster(data = swe_elev, maxcell = Inf , show.legend =TRUE ) +
-  ggspatial::annotation_scale(location = 'tl',width_hint = .4)+
+  geom_spatraster(data = swe_elev,na.rm = TRUE, maxcell = Inf, show.legend = TRUE) +
+  ggspatial::annotation_scale(location = 'tl',width_hint = .6,text_cex = 2)+
   scale_fill_hypso_tint_c(limits = elev_limits , palette = "dem_poster",alpha = 0.4,direction = 1)+ # Add tinted alpha layer to hill shaded layer
   geom_sf(data=lakes_sweden , fill  = "blue", alpha = .3 ,colour = NA)+
   geom_point(data=meta_se , aes(longitude_WGS84 , latitude_WGS84 , 
                                 shape = malaise_trap_type),
-                                size=2.3, colour = "white")+
+                                size=6, colour = "white")+
   geom_point(data=meta_se , aes(longitude_WGS84 , latitude_WGS84 , 
-                                colour = NILS_mhabitat, shape = malaise_trap_type),
-                              size=2)+
+                                colour = trap_habitat, shape = malaise_trap_type),
+                              size=5)+
   scale_colour_manual(values = tcols)+
-  theme_linedraw()+
+  theme_linedraw(base_size = 25)+
   labs(x = "Longitude" ,
                         y = "Latitude" , 
-                        fill = "Elevation",
+                        fill = "Elevation (m)",
                         shape = "Trap-type",
                         colour = "Trap habitat")+
   theme(legend.key = element_rect(fill = "white"),
         legend.position = "left",
-        legend.direction = "vertical")
+        legend.direction = "vertical",
+        legend.key.width = unit(1.5, "cm"),
+        legend.key.height = unit(1.5, "cm"))+
+  guides(color=guide_legend(override.aes=list(fill=NA,size=6)),
+         shape=guide_legend(override.aes=list(fill=NA,size=6)))
 
 p2 <- mad_hshade_plot + # PLot hillshaded map
-  geom_spatraster(data = mad_elev, maxcell = Inf , show.legend = FALSE) +
-  ggspatial::annotation_scale(location = 'tl',width_hint = .4)+
+  geom_spatraster(data = mad_elev, maxcell = Inf , show.legend = TRUE) +
+  ggspatial::annotation_scale(location = 'tl',width_hint = .6,text_cex = 2)+
   geom_point(data=meta_mg , aes(longitude_WGS84 , latitude_WGS84 , 
                                 shape = malaise_trap_type),
-             size=2.3, colour = "white")+
+             size=6, colour = "white")+
   geom_point(data=meta_mg , aes(longitude_WGS84 , latitude_WGS84 , 
-                                colour = trap_habitat, shape = malaise_trap_type),
-             size=2)+  scale_fill_hypso_tint_c(limits = elev_limits , palette = "dem_poster",alpha = 0.4,direction = 1)+
-  scale_colour_viridis_d(option="mako")+
-  theme_linedraw()+
+                                colour = trap_habitat, shape = malaise_trap_type),size=5)+ 
+  scale_fill_hypso_tint_c(limits = elev_limits , palette = "dem_poster",alpha = 0.4,direction = 1)+
+  scale_colour_viridis_d(option="mako", end=.8)+
+  theme_linedraw(base_size = 25)+
   labs(x = "Longitude" ,
        y = "Latitude" , 
        fill = "Elevation",
        shape = "Trap-type",
-       colour = "Trap habitat")
-
-
-
+       colour = "Trap habitat")+
+  theme(legend.key = element_rect(fill = "white"),
+        legend.position = "right",
+        legend.direction = "vertical",
+        legend.key.width = unit(1.5, "cm"),
+        legend.key.height = unit(1.5, "cm"))+
+  guides(color=guide_legend(override.aes=list(fill=NA,size=6)),
+         shape=guide_legend(override.aes=list(fill=NA,size=6)))
 
 
 
 # plot ----------------------------------------------------------------------------------------
+tiff("figures/maps.tiff", width = 2000, height = 1000, compression = "lzw")
+p1 + p2  & labs(x = "Longitude" , y = "Latitude" , fill = "Elevation", shape = "Trap-type") 
+dev.off()
 
-p1 + p2 + plot_layout(guides="collect") & labs(x = "Longitude" ,
-                                               y = "Latitude" , 
-                                               fill = "Elevation",
-                                               shape = "Trap-type")&
-  theme(legend.position = "none")
-
+browseURL("figures/maps.tiff")
 
 
 
